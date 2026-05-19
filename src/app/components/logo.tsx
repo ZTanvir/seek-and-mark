70a7b5fd94ae56ac61c515595a9d492f883616c8
zahirul <zahirulopel@gmail.com>
2026-05-19 10:52:53 +0600

    Add logo

---
+++ src/app/components/logo.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+export default function Logo() {
+  return (
+    <Link className="font-bold text-xl" href="/">
+      Seek And Mark
+    </Link>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Logo from "./components/logo";
 export default function Home() {
   return <div className="flex flex-col flex-1"></div>;
 }
