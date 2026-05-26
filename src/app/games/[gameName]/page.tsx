485133d1b5b2e7c4e6b3e0205d49cfadd7003546
zahirul <zahirulopel@gmail.com>
2026-05-26 12:47:32 +0600

    Add custom cursor image

--- src/app/games/[gameName]/page.tsx
+++ src/app/games/[gameName]/page.tsx
@@ -6,7 +6,7 @@ export default async function GamePlayPage({
   const { gameName } = await params;
   console.log(gameName);
   return (
-    <div>
+    <div className="cursor-custom w-full h-full ">
       <h1>Robot city</h1>
     </div>
   );
