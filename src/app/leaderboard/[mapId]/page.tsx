13b49bc58d2e071c8c5308636ea6024c064ed382
zahirul <zahirulopel@gmail.com>
2026-06-23 09:40:59 +0600

    Add table raw data

--- src/app/leaderboard/[mapId]/page.tsx
+++ src/app/leaderboard/[mapId]/page.tsx
@@ -1,3 +1,5 @@
+import { Trophy } from "lucide-react";
+
 type LeaderboardMapPageProps = {
   params: Promise<{ mapId: string }>;
 };
@@ -7,5 +9,40 @@ export default async function LeaderboardMapPage({
 }: LeaderboardMapPageProps) {
   const { mapId } = await params;
   console.log({ mapId });
-  return <div>page</div>;
+  return (
+    <div>
+      <h1 className="flex items-center gap-2 text-xl md:text-3xl">
+        <span>
+          <Trophy />
+        </span>
+        Top achievers
+        <span>
+          <Trophy />
+        </span>
+      </h1>
+      <table className="text-md w-full table-auto text-left text-gray-500">
+        <thead className="w-full border-b border-gray-200 bg-gray-50 text-sm font-semibold text-gray-700 uppercase">
+          <tr>
+            <th scope="col" className="px-6 py-4">
+              Rank
+            </th>
+            <th scope="col" className="px-6 py-4">
+              Name
+            </th>
+            <th scope="col" className="px-6 py-4">
+              Time
+            </th>
+            <th scope="col" className="px-6 py-4">
+              Map
+            </th>
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-200 bg-white">
+          <tr className="transition-colors hover:bg-gray-50">
+            <td className="px-6 py-4 font-medium text-gray-900">John Doe</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
