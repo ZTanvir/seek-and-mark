192aebb86ce4617b9688e132bd1a294b5391bc05
zahirul <zahirulopel@gmail.com>
2026-06-28 13:31:54 +0600

    Wrap leadboard table to suspence so user will see some navbar before loading

--- src/app/maps/[mapId]/leaderboard/page.tsx
+++ src/app/maps/[mapId]/leaderboard/page.tsx
@@ -3,6 +3,9 @@ import { getTopLeaderboardData, leaderboardCount } from "@/lib/dal/db-query";
 import { dateTimeToString } from "@/lib/utils";
 import Pagination from "@/components/pagination";
 import Navbar from "@/components/navbar";
+import { Suspense } from "react";
+import LeaderboardTable from "@/components/leaderboard-table";
+import Loading from "../loading";
 
 type LeaderboardMapPageProps = {
   params: Promise<{ mapId: string }>;
@@ -17,13 +20,6 @@ export default async function LeaderboardMapPage({
   const queryParams = await searchParams;
   const page = Number(queryParams.page) || 1;
   const limit = Number(queryParams.limit) || 10;
-  const skipData = limit * (page - 1);
-  const leaderboardData = await getTopLeaderboardData(
-    limit,
-    Number(mapId),
-    skipData,
-  );
-  const totalLeaderboardData = await leaderboardCount();
   return (
     <div>
       <header>
@@ -38,53 +34,9 @@ export default async function LeaderboardMapPage({
           <Trophy color="gold" />
         </span>
       </h1>
-      <table className="text-md w-full table-auto text-left text-gray-500">
-        <thead className="w-full border-b border-gray-200 bg-gray-50 text-sm font-semibold text-gray-700 uppercase">
-          <tr>
-            <th scope="col" className="px-6 py-4">
-              Rank
-            </th>
-            <th scope="col" className="px-6 py-4">
-              Name
-            </th>
-            <th scope="col" className="px-6 py-4">
-              Time
-            </th>
-            <th scope="col" className="px-6 py-4">
-              Map
-            </th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-200 bg-white">
-          {leaderboardData &&
-            leaderboardData.map((leader, index) => (
-              <tr
-                key={leader.id}
-                className="transition-colors hover:bg-gray-50"
-              >
-                <td className="px-6 py-4 font-medium text-gray-900">
-                  {index + 1}
-                </td>
-                <td className="px-6 py-4 font-medium text-gray-900">
-                  {leader.username}
-                </td>
-                <td className="px-6 py-4 font-medium text-gray-900">
-                  {dateTimeToString(leader.endTime)}
-                </td>
-                <td className="px-6 py-4 font-medium text-gray-900 capitalize">
-                  {leader.map.name}
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
-      {leaderboardData && totalLeaderboardData && (
-        <Pagination
-          totalData={totalLeaderboardData}
-          dataPerPage={limit}
-          currentPage={page}
-        />
-      )}
+      <Suspense fallback={<Loading />}>
+        <LeaderboardTable mapId={mapId} page={page} limit={limit} />
+      </Suspense>
     </div>
   );
 }
---
+++ src/components/leaderboard-table.tsx
@@ -0,0 +1,74 @@
+import { getTopLeaderboardData, leaderboardCount } from "@/lib/dal/db-query";
+import Pagination from "./pagination";
+import { dateTimeToString } from "@/lib/utils";
+
+type LeaderboardTableProps = {
+  mapId: string;
+  page: number;
+  limit: number;
+};
+export default async function LeaderboardTable({
+  mapId,
+  page,
+  limit,
+}: LeaderboardTableProps) {
+  const skipData = limit * (page - 1);
+  const leaderboardData = await getTopLeaderboardData(
+    limit,
+    Number(mapId),
+    skipData,
+  );
+  const totalLeaderboardData = await leaderboardCount();
+
+  return (
+    <div>
+      <table className="text-md w-full table-auto text-left text-gray-500">
+        <thead className="w-full border-b border-gray-200 bg-gray-50 text-sm font-semibold text-gray-700 uppercase">
+          <tr>
+            <th scope="col" className="px-6 py-4">
+              Rank
+            </th>
+            <th scope="col" className="px-6 py-4">
+              Name
+            </th>
+            <th scope="col" className="px-6 py-4">
+              Time
+            </th>
+            <th scope="col" className="px-6 py-4">
+              Map
+            </th>
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-200 bg-white">
+          {leaderboardData &&
+            leaderboardData.map((leader, index) => (
+              <tr
+                key={leader.id}
+                className="transition-colors hover:bg-gray-50"
+              >
+                <td className="px-6 py-4 font-medium text-gray-900">
+                  {index + 1}
+                </td>
+                <td className="px-6 py-4 font-medium text-gray-900">
+                  {leader.username}
+                </td>
+                <td className="px-6 py-4 font-medium text-gray-900">
+                  {dateTimeToString(leader.endTime)}
+                </td>
+                <td className="px-6 py-4 font-medium text-gray-900 capitalize">
+                  {leader.map.name}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      {leaderboardData && totalLeaderboardData && (
+        <Pagination
+          totalData={totalLeaderboardData}
+          dataPerPage={limit}
+          currentPage={page}
+        />
+      )}
+    </div>
+  );
+}
