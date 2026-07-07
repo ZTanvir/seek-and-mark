e2e05552795e533d5c7d4d7586c4f583a3fd532c
zahirul <zahirulopel@gmail.com>
2026-07-07 12:12:38 +0600

    Update SingInSchema and add custom error message for email check

--- src/components/signin-form.tsx
+++ src/components/signin-form.tsx
@@ -21,34 +21,60 @@ export default function SignInForm() {
     initialSignInState,
   );
   const [errors, setErrors] = useState<null | FormErrors>(null);
+
   const handleForm = (event: React.SubmitEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.target);
-
-    const result = SignInSchema.safeParse(formData);
+    const rawData = {
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+    console.log("rawdata", rawData);
+    const result = SignInSchema.safeParse(rawData);
     if (!result.success) {
       const flattened = z.flattenError(result.error);
+      console.log(flattened);
       setErrors(flattened.fieldErrors);
     } else {
+      setErrors(null);
     }
   };
   return (
-    <form onSubmit={handleForm}>
-      <div>
+    <form className="space-y-2" onSubmit={handleForm}>
+      <div className="space-y-1">
         <label htmlFor="email">Email address</label>
-        <input type="text" name="email" id="email" />
-        {errors?.email && <p>{errors.email[0]}</p>}
+        <input
+          className="w-full rounded-lg border border-gray-500 px-3 py-1"
+          type="text"
+          name="email"
+          id="email"
+        />
+        {errors?.email && <p className="text-red-400">{errors.email[0]}</p>}
       </div>
-      <div>
+      <div className="space-y-1">
         <label htmlFor="password">Password</label>
-        <input type="text" name="password" id="password" />
-        {errors?.password && <p>{errors.password[0]}</p>}
+        <input
+          className="w-full rounded-lg border border-gray-500 px-3 py-1"
+          type="text"
+          name="password"
+          id="password"
+        />
+        {errors?.password && (
+          <p className="text-red-400">{errors.password[0]}</p>
+        )}
       </div>
-      <button disabled={isPending} type="submit">
+      <button
+        className="mx-auto cursor-pointer rounded-lg bg-blue-800 px-6 py-2 text-white transition-colors duration-300 hover:bg-blue-700 focus:bg-blue-700"
+        disabled={isPending}
+        type="submit"
+      >
         LOGIN
       </button>
       <p>
-        Don&apos;t have an account? <Link href="/register">Sign-up</Link>
+        Don&apos;t have an account?{" "}
+        <Link href="/register" className="text-blue-500 underline">
+          Sign-up
+        </Link>
       </p>
     </form>
   );
--- src/lib/zod-schemas/auth-schema.ts
+++ src/lib/zod-schemas/auth-schema.ts
@@ -1,6 +1,21 @@
 import * as z from "zod";
 
 export const SignInSchema = z.object({
-  email: z.email(),
-  password: z.string().min(6, "Password must be at least 6 characters long."),
+  email: z
+    .string()
+    .trim()
+    .pipe(
+      z.email({
+        error: (issue) => {
+          if (issue.input === "") {
+            return "Email address required";
+          }
+          return "Invalid email address";
+        },
+      }),
+    ),
+  password: z
+    .string()
+    .trim()
+    .min(6, "Password must be at least 6 characters long."),
 });
