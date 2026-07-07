0a641cf1d83d726533a53a6a0bfb0bd1c7de8789
zahirul <zahirulopel@gmail.com>
2026-07-07 11:05:28 +0600

    Add style to signInPage

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,10 +1,12 @@
 import SignInForm from "@/components/signin-form";
 export default function SigninPage() {
   return (
-    <section>
-      <h1>Welcome back</h1>
-      <p>Please enter your details</p>
-      <SignInForm />
-    </section>
+    <div>
+      <section className="mx-auto max-w-lg space-y-1 rounded-xl bg-neutral-100 px-6 py-4">
+        <h1 className="text-center text-xl md:text-3xl">Welcome back</h1>
+        <p className="text-center">Please enter your details</p>
+        <SignInForm />
+      </section>
+    </div>
   );
 }
