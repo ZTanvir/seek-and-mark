cc3c559f034329d01f69a2917efe1ce686cc2182
zahirul <zahirulopel@gmail.com>
2026-07-09 10:09:08 +0600

    Validate signup form in the frontend

--- src/app/(auth)/signup/_components/signup-form.tsx
+++ src/app/(auth)/signup/_components/signup-form.tsx
@@ -1,9 +1,19 @@
 "use client";
 
+import { SignUpSchema } from "@/lib/zod-schemas/auth-schema";
 import Link from "next/link";
+import * as z from "zod";
+import { useState } from "react";
+
+type SignUpErrors = {
+  username?: string[];
+  email?: string[];
+  password?: string[];
+};
 
 export default function SignUpForm() {
   const isPending = false;
+  const [errors, setErrors] = useState<SignUpErrors | null>(null);
 
   const handleSubmitForm = (event: React.SubmitEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -13,6 +23,13 @@ export default function SignUpForm() {
       email: formData.get("email"),
       password: formData.get("password"),
     };
+    const result = SignUpSchema.safeParse(rawData);
+    if (!result.success) {
+      const formatErrors = z.flattenError(result.error);
+      setErrors(formatErrors.fieldErrors);
+    } else {
+      console.log(result.data);
+    }
     console.log(rawData);
   };
   return (
@@ -20,7 +37,9 @@ export default function SignUpForm() {
       className="mx-auto mt-[200px] max-w-lg space-y-2 rounded-xl bg-neutral-100 px-6 py-4"
       onSubmit={handleSubmitForm}
     >
-      <legend className="text-center text-3xl font-bold">Sign up</legend>
+      <legend className="text-center text-xl font-bold md:text-3xl">
+        Sign up
+      </legend>
       <div className="space-y-1">
         <label htmlFor="username">Username</label>
         <input
@@ -29,6 +48,9 @@ export default function SignUpForm() {
           name="username"
           id="username"
         />
+        {errors?.username && (
+          <p className="text-red-400">{errors.username[0]}</p>
+        )}
       </div>
       <div className="space-y-1">
         <label htmlFor="email">Email</label>
@@ -38,6 +60,7 @@ export default function SignUpForm() {
           name="email"
           id="email"
         />
+        {errors?.email && <p className="text-red-400">{errors.email[0]}</p>}
       </div>
       <button
         className="flex cursor-pointer items-center gap-x-2 rounded-lg bg-blue-800 px-6 py-2 text-white transition-colors duration-300 hover:bg-blue-700 focus:bg-blue-700"
--- src/lib/zod-schemas/auth-schema.ts
+++ src/lib/zod-schemas/auth-schema.ts
@@ -14,8 +14,26 @@ export const SignInSchema = z.object({
         },
       }),
     ),
+  password: z.string().trim().min(1, "Password required"),
+});
+
+export const SignUpSchema = z.object({
+  username: z.string().trim().min(1, "Username required"),
+  email: z
+    .string()
+    .trim()
+    .pipe(
+      z.email({
+        error: (issue) => {
+          if (issue.input === "") {
+            return "Email address required";
+          }
+          return "Invalid email address";
+        },
+      }),
+    ),
   password: z
     .string()
     .trim()
-    .min(6, "Password must be at least 6 characters long."),
+    .min(8, "Password must be at least 8 characters long."),
 });
