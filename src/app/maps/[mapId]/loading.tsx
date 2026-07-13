44cbc9f7d36fddfa31c6ae7505d218f051d3dc47
zahirul <zahirulopel@gmail.com>
2026-07-13 12:46:59 +0600

    Position loader in the center of screen on loading the map/id page

--- src/app/maps/[mapId]/loading.tsx
+++ src/app/maps/[mapId]/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex h-full w-full items-center justify-center">
+    <div className="flex min-h-screen w-full items-center justify-center">
       <div className="h-12 w-12 animate-spin rounded-full border-4 border-gray-200 border-t-purple-400"></div>
       <span className="sr-only">Loading...</span>
     </div>
