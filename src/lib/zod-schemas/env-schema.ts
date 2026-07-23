b713f3148953536176780658bf64dfea1f718d56
zahiru <zahirulopel@gmail.com>
2026-07-23 10:42:22 +0600

    Add AUTH_SECRET as enviroment variable

--- src/lib/zod-schemas/env-schema.ts
+++ src/lib/zod-schemas/env-schema.ts
@@ -3,4 +3,5 @@ import * as z from "zod";
 export const envSchema = z.object({
   DATABASE_URL: z.url(),
   SUPABASE_STORAGE_BASE_URL: z.url(),
+  AUTH_SECRET:z.string().min(32,"Must be at least 32 characters long.")
 });
