004819817afec44abf01ba1a721aeff32d5c0d49
zahiru <zahirulopel@gmail.com>
2026-07-23 11:24:30 +0600

    Add smooth transition effect on open and close modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -20,7 +20,7 @@ export default function Modal({ children, isOpen }: ModalProps) {
 
   return (
     <dialog
-      className="fixed m-auto rounded-lg border-2 border-purple-500 backdrop:bg-black/50 backdrop:backdrop-blur-sm"
+      className="fixed m-auto opacity-0 hidden transition:[display,opacity,translate] transition-discrete translate-x-0 translate-y-12.5 duration-700 rounded-lg border-2 border-purple-500 backdrop:bg-black/50 backdrop:backdrop-blur-sm open:block open:opacity-100 open:translate-[0,0] @starting-style:opacity-0 @starting-style:translate-x-0 @starting-style:-translate-y-12.5 "
       ref={dialogRef}
     >
       {children}
